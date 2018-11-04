2327c91b776a58f3ef7e836f1a60f0197b9ef019
ReturnZero23 <raofei23@gmail.com>
2018-11-04 16:31:28 +0800

    修改着色器增加贴图渲染

--- shaders/fragmentshader.fs
+++ shaders/fragmentshader.fs
@@ -1,7 +1,12 @@
 #version 330 core
 out vec4 FragColor;
-in vec3 outColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture;
+
 void main()
 {
-   FragColor = vec4(outColor,1.0);
+    FragColor = texture(ourTexture, TexCoord) * 0.5 + vec4(ourColor,1.0) * 0.5;
 }
